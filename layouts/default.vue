d02ae6875ae6fdf3dcb23190a075a2d1fbbd060c
Etienne RABY <etienne.raby@fittingbox.com>
2020-12-06 16:40:48 +0100

    responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="bg-gray-50 min-h-screen">
     <nav
-      class="bg-yellow-600 text-white flex justify-between items-center px-1 md:px-5 py-2 shadow-lg"
+      class="relative bg-yellow-600 text-white flex justify-between items-center px-1 sm:px-5 py-2 shadow-lg"
     >
+      <div
+        class="absolute inset-y-0 left-1/2 transform -translate-x-1/2 flex items-center sm:space-x-2"
+      >
+        <svg
+          class="hidden sm:block h-10"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <NuxtLink class="text-xl sm:text-3xl whitespace-nowrap" to="/"
+          >Find My Doggo</NuxtLink
+        >
+      </div>
       <svg
         class="h-8"
         fill="none"
@@ -17,26 +38,7 @@
           d="M4 6h16M4 12h16M4 18h16"
         ></path>
       </svg>
-      <div class="flex items-center space-x-2">
-        <svg
-          class="h-10"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          ></path>
-        </svg>
-        <NuxtLink class="text-3xl whitespace-nowrap" to="/"
-          >Find My Doggo</NuxtLink
-        >
-      </div>
-      <div class="flex items-center space-x-3">
+      <div class="hidden sm:flex items-center space-x-3">
         <template v-if="!user">
           <NuxtLink to="/login">Login</NuxtLink>
           <NuxtLink class="whitespace-nowrap" to="/signup">Sign up</NuxtLink>
@@ -61,7 +63,7 @@
         </svg>
       </div>
     </nav>
-    <div class="container p-5">
+    <div class="container p-5 mx-auto">
       <Nuxt></Nuxt>
     </div>
   </div>
