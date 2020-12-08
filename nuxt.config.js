6fb0a827f1edc30fde97961c047bb378dfeb1203
Etienne RABY <etienne.raby@fittingbox.com>
2020-12-08 12:23:51 +0100

    added meta description for SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Find the doggos that suits you (spoiler, all of them).',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
