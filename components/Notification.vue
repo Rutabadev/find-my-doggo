0d19036767fbf360f0b256fa53523753a8480140
Etienne RABY <etienne.raby@fittingbox.com>
2020-12-10 18:19:12 +0100

    notification slightly up

--- components/Notification.vue
+++ components/Notification.vue
@@ -1,6 +1,7 @@
 <template>
   <span
     class="w-3 h-3 inline-block relative rounded-full bg-secondary-500 ml-4 -mr-8"
+    style="margin-bottom: 1px"
   >
     <span
       class="w-3 h-3 absolute inset-0 rounded-full bg-secondary-500 animate-ping"
