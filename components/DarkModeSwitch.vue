f95ae566ed1f821fe4efc10b4eb460dc9b0f1755
Etienne RABY <etienne.raby@fittingbox.com>
2020-12-10 22:19:31 +0100

    moon sun dark theme

--- components/DarkModeSwitch.vue
+++ components/DarkModeSwitch.vue
@@ -1,28 +1,16 @@
 <template>
-  <button class="px-3 cursor-pointer" @click="toggleDarkMode">
-    <svg
-      class="h-7 w-14"
-      width="42"
-      height="24"
-      viewBox="0 0 42 24"
-      xmlns="http://www.w3.org/2000/svg"
+  <button class="dark-mode-switch px-3 cursor-pointer" @click="toggleDarkMode">
+    <div
+      class="rounded-full w-9 h-6 text-secondary-500 bg-gray-50 dark:text-blue-400 dark:bg-gray-800 transition relative"
     >
-      <rect
-        x="0"
-        y="0"
-        width="42"
-        height="24"
-        rx="12"
-        :fill="isDarkMode ? '#1f2937' : 'grey'"
-      />
-      <circle
-        style="transition: all 150ms ease"
-        :cx="isDarkMode ? 29 : 13"
-        cy="12"
-        r="10"
-        fill="white"
-      />
-    </svg>
+      <div
+        class="absolute transition-transform transform-gpu"
+        :class="isDarkMode ? 'translate-x-1/2' : 'translate'"
+      >
+        <Moon v-if="isDarkMode"></Moon>
+        <Sun v-else></Sun>
+      </div>
+    </div>
   </button>
 </template>
 
