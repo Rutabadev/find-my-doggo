6637b25bf35595a1d05ca878ea1c8d71a2c5b04e
Rutabadev <etienner37@gmail.com>
2020-12-12 20:12:21 +0100

    fix dropdown anim

--- components/LangDropdown.vue
+++ components/LangDropdown.vue
@@ -27,11 +27,11 @@
     </div>
     <transition
       enter-active-class="transition ease-out duration-100"
-      enter-class="opacity-0 scale-95"
-      enter-to-class="opacity-100 scale-100"
+      enter-class="transform opacity-0 scale-y-90"
+      enter-to-class="transform opacity-100 scale-y-100"
       leave-active-class="transition ease-in duration-75"
-      leave-class="opacity-100 scale-100"
-      leave-to-class="opacity-0 scale-95"
+      leave-class="transform opacity-100 scale-y-100"
+      leave-to-class="transform opacity-0 scale-y-90"
     >
       <div
         v-show="langDropdownOpen"
