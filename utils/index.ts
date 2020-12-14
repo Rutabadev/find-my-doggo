8704f9afc68f593576e2780ee68c6635ab93f964
Rutabadev <etienner37@gmail.com>
2020-12-14 17:53:41 +0100

    removed old util

--- utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const parseRequestErrors = (err: any): string[] => {
-  return (
-    err?.response?.data?.errors?.map(({ message }: any) => message) ?? [
-      err?.response?.data ?? err?.message ?? 'Server unavailable',
-    ]
-  )
-}
