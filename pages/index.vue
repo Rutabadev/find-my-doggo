f0069515e5382d81f5fed1b9fcd7ce359234f992
Rutabadev <etienner37@gmail.com>
2020-12-15 22:06:54 +0100

    align home page text center

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="flex flex-col items-center">
-    <h1 class="text-xl mb-8">
+    <h1 class="text-xl mb-8 text-center">
       {{ $t('index.welcome') }}
       <em>{{ $t('index.spoiler') }}</em>
     </h1>
     <Dog class="h-64 w-64"></Dog>
-
     <button class="button">
       <NuxtLink to="/find">{{ $t('index.find') }}</NuxtLink>
     </button>
