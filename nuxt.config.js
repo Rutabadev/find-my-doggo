1d3e2bccff606a8e552afd8af39457ae71e2a5da
Rutabadev <etienner37@gmail.com>
2020-12-16 22:31:48 +0100

    dog shows version

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  env: {
+    version: JSON.stringify(require('./package.json').version),
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'find-my-doggo',
