4d246b17ff5103f0938a416c181f03052e246a6d
Rutabadev <etienner37@gmail.com>
2021-05-12 16:06:12 +0200

    fix global error spacing in form

--- components/form/index.vue
+++ components/form/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="max-w-sm mt-10 mx-auto rounded-lg bg-white dark:bg-gray-700 transition shadow-md p-5 flex flex-col"
+    class="max-w-sm mt-10 mx-auto rounded-lg bg-white dark:bg-gray-700 transition shadow-md p-5 flex flex-col gap-5"
   >
-    <h1 class="text-2xl text-center mb-12">{{ title }}</h1>
+    <h1 class="text-2xl text-center">{{ title }}</h1>
     <FormErrors :errors="errors"></FormErrors>
-    <form class="grid gap-8" @submit.prevent="$emit('submit')">
+    <form class="mt-4 grid gap-8" @submit.prevent="$emit('submit')">
       <slot></slot>
     </form>
-    <div class="ml-auto mt-5 text-right">
+    <div class="ml-auto text-right">
       <slot name="bottom"></slot>
     </div>
   </div>
