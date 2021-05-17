ae6263a0085e27773dbc80e6b9da5dea89b3ca4c
Rutabadev <etienner37@gmail.com>
2021-05-17 23:58:10 +0200

    order langs + add admin texts

--- i18n/fr.js
+++ i18n/fr.js
@@ -1,6 +1,34 @@
 export default {
+  create_account: {
+    title: 'Enregistrement',
+    username: "Nom d'utilisateur",
+    email: 'Email',
+    password: 'Mot de passe',
+    create: "S'enregistrer",
+    creating: 'Enregistrement...',
+    already: 'Déjà un compte ? Aller au ',
+    login: 'login',
+  },
   dark_mode_reset:
     'Voulez vous remettre le thème en fonction de votre préférence système ?',
+  edit_account: {
+    title: 'Informations du compte',
+    update: 'Mettre à jour',
+    updating: 'Mise à jour...',
+    delete: 'Supprimer mon compte',
+    deleting: 'Suppression...',
+    sure_delete: 'Etes vous sûr ?',
+    cancel_delete: 'Annuler',
+    confirm_delete: 'Supprimer',
+    delete_success: 'Compte supprimé.',
+  },
+  forgot_password: {
+    title: 'Demande de réinitialisation de votre mot de passe',
+    send: 'envoyer un email',
+    sending: 'envoi',
+    check_email:
+      "Vérifier votre boite mail pour réinitialiser votre mot de passe (c'est probablement dans les spams).",
+  },
   index: {
     title: 'Accueil',
     welcome: 'Trouve le doggo qui te correspond le mieux',
@@ -17,36 +45,8 @@ export default {
     signup: "l'enregistrement",
     forgot: 'Mot de passe oublié ?',
   },
-  create_account: {
-    title: 'Enregistrement',
-    username: "Nom d'utilisateur",
-    email: 'Email',
-    password: 'Mot de passe',
-    create: "S'enregistrer",
-    creating: 'Enregistrement...',
-    already: 'Déjà un compte ? Aller au ',
-    login: 'login',
-  },
-  edit_account: {
-    title: 'Informations du compte',
-    update: 'Mettre à jour',
-    updating: 'Mise à jour...',
-    delete: 'Supprimer mon compte',
-    deleting: 'Suppression...',
-    sure_delete: 'Etes vous sûr ?',
-    cancel_delete: 'Annuler',
-    confirm_delete: 'Supprimer',
-    delete_success: 'Compte supprimé.',
-  },
   logout: 'Déconnexion',
   find: 'Ceci est une page protégée',
-  forgot_password: {
-    title: 'Demande de réinitialisation de votre mot de passe',
-    send: 'envoyer un email',
-    sending: 'envoi',
-    check_email:
-      "Vérifier votre boite mail pour réinitialiser votre mot de passe (c'est probablement dans les spams).",
-  },
   reset_password: {
     title: 'Réinitialiser mot de passe',
     reset: 'Réinitialiser',
